f661de557f016e5ded3eb1d08d242c42db9ded11
Alexandre <alexandre.sicavalcante@gmail.com>
2026-08-06 22:49:34 -0300

    aula 06_08

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import './App.css'
+
+
+import styles from './App.module.css'
 
 function App() {
   return (
-    <div>
+    <div className={styles.body}>
       <h1>Home</h1>
       <p>Exemplo parágrafo</p>
-      <h2>Exemplo h2</h2>
+      <h2 className={styles.titulo2}>Exemplo h2</h2>
       <h3>Exemplo h3</h3>
       <label htmlFor="">Exemplo label</label>
     </div>
